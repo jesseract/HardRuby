9f0a0cc633ca1fb893ca96562c67b41e22ec3eac
Jesseract <jlpearce@gmail.com>
2016-10-20 15:50:36 -0400

    Lessons 1-5

---
+++ ex5.rb
@@ -0,0 +1,21 @@
+name = 'Jessa L. Pearce'
+age = 37 # not a lie in 2016
+height = 60 #inches
+height_in_cm = height * 2.54
+weight = 130 #lbs
+weight_in_kg = weight * 0.454
+eyes = 'Green'
+teeth = 'White'
+hair = 'Fall rainbow'
+
+puts "Let's talk about #{name}."
+puts "She's #{height} inches tall."
+puts "Her height in metric is #{height_in_cm} centimeters."
+puts "She's #{weight} pounds heavy."
+puts "Actually, that's not too heavy."
+puts "In kg, she's #{weight_in_kg}."
+puts "She's got #{eyes} eyes and #{hair} hair."
+puts "Her teeth are usually #{teeth} depending on the coffee."
+
+#this line is tircky, try to get it exactly right
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
