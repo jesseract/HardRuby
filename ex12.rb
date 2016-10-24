02f5625cdcdc176a53cd7a1e1bfe8f0f5ff174ac
Jesseract <jlpearce@gmail.com>
2016-10-24 17:37:52 -0400

    Examples 11-15

---
+++ ex12.rb
@@ -0,0 +1,20 @@
+# print "Give me a number: "
+# number = gets.chomp.to_i
+#
+# bigger = number * 100
+# puts "A bigger number is #{bigger}."
+#
+# print "Give me another number: "
+# another = gets.chomp
+# number = another.to_i
+#
+# smaller = number / 100
+# puts "A smaller number is #{smaller}."
+
+
+
+print "Give me some money! "
+cash = gets.chomp.to_f
+change = cash * 0.10
+
+puts "Here's your change: #{change}"
